f689840141acdc356e785fde6c79555f5e68ecce
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-11-24 22:48:41 +0100

    Create pibackup_adhoc.sh

---
+++ pibackup_adhoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Adhoc version - pass RasPi system hostname(s) to script as commandline argument
+DEST="/mnt/diskstation1-Daten/Backup/PiBackup"
+
+if test "$#" -lt 1; then
+        echo "Illegal number of parameters. At least one hostname must be provided."
+else
+        DATE=`date +%Y-%m-%d`
+        for i in "$@"; do
+                ssh root@${i} "sudo dd if=/dev/mmcblk0 bs=1M | gzip -" | dd of=${DEST}/pibackup_${i}_${DATE}.gz
+                echo $i 
+        done
+
+fi
