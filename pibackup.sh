c1a43b05951d9b145dc89588f92d93fe73bac032
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-11-24 22:49:32 +0100

    Update pibackup.sh

--- pibackup.sh
+++ pibackup.sh
@@ -3,7 +3,7 @@
 # The resulting image can be restored to SD Card using Etcher or so
 # I use it for automated backups using cron
 
-DEST="/mnt/diskstation1-Daten/Backup/PiBackup"
+DEST='/mnt/diskstation1-Daten/Backup/PiBackup'
 
 DATE=`date +%Y-%m-%d`
 for i in hostname1 hostname2; do
