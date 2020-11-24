a08dad6a7e3215b21fd7b6a225c9cb573ec1e2a4
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-11-24 22:43:01 +0100

    Create pibackup.sh

---
+++ pibackup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Make running hot image backup of RaspberryPi
+# The resulting image can be restored to SD Card using Etcher or so
+# I use it for automated backups using cron
+
+DEST=/mnt/diskstation1-Daten/Backup/PiBackup
+
+DATE=`date +%Y-%m-%d`
+for i in hostname1 hostname2; do
+        ssh root@${i} "sudo dd if=/dev/mmcblk0 bs=1M | gzip -" | dd of=${DEST}/pibackup_${i}_${DATE}.gz
+done
+
