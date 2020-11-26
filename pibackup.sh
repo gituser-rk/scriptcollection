f646f96441a3fbd232224ba15546105c9a83c235
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-11-26 18:28:46 +0100

    Update pibackup.sh

--- pibackup.sh
+++ pibackup.sh
@@ -4,10 +4,14 @@
 # I use it for automated backups using cron
 # (c) Ronny Kröhnert - gituser_rk 2020
 
-DEST='/mnt/diskstation1-Daten/Backup/PiBackup'
+DEST=/mnt/path/to/backup/directory # Backup destination path. No trailing slash!
+KEEP=3 # Cleanup: how many versions to keep
 
 DATE=`date +%Y-%m-%d`
-for i in hostname1 hostname2; do
+for i in hostname1 hostname2 another-hostname; do
+        # make hot backup of a pi(e)
         ssh root@${i} "sudo dd if=/dev/mmcblk0 bs=1M | gzip -" | dd of=${DEST}/pibackup_${i}_${DATE}.gz
+        # cleanup of old backups, only keep the recent 3 versions
+        find ${DEST}/pibackup_${i}_*.gz -mtime +${KEEP} -exec rm -f {} \;
 done
 
